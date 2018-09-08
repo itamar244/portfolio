5a97ee8d173ce4f4a6fec6143625088b5393f85a
itamar244 <itamary24@gmail.com>
2018-09-08 11:30:28 +0300

    add polyfill for web components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import { onDomLoaded, query, queryAll, removeElement } from './dom-utils';
 import initApp from './app-controller';
 
-import './elements';
-
 import './styles/main.less';
 
 if (module.hot) {
   module.hot.accept(initApp);
 }
 
+const elementsLoadPromise = new Promise(resolve => {
+  if (!('CustomElementRegistry' in window)) {
+    import('@webcomponents/custom-elements').then(() => {
+      import(/* webpackMode: 'eager' */'./elements').then(resolve);
+      resolve();
+    });
+  } else {
+    import(/* webpackMode: 'eager' */'./elements').then(resolve);
+  }
+});
+
 onDomLoaded(() => {
   document.body.classList.add('ready');
-  initApp();
+  elementsLoadPromise.then(initApp);
 
   setTimeout(() => {
     removeElement(query('#loading'));
