860cc1addb557fba6491c21c5d04751313d5790b
itamar244 <itamary24@gmail.com>
2018-09-10 13:17:25 +0300

    refactor(header): use attribute data-route instead of name for routing

--- src/app-controller.js
+++ src/app-controller.js
@@ -3,39 +3,40 @@ import { setLanguage, getCurLanguage } from './texts';
 import { guard } from './utils';
 import Router from './router';
 
-const getLink = name => query(`.header__link[name=${name}]`);
-const getLinks = () => queryAll('.header__link');
+const getLink = name => query(`.header [data-route=${name}]`);
+const getLinks = () => queryAll('.header [data-route]');
 const getContent = name => query(`.content--${name}`);
 const getActiveContent = () => query('.content.active');
 
 export default function initApp() {
   const router = new Router({
-    defaultPage: 'about',
+    defaultPage: '',
     defaultParams: { lang: 'EN' },
     initFirstRoute: true,
     onRouteChange: route => {
-      const link = getLink(route);
+      query('.header').classList.toggle('fixed', route !== '');
 
-      if (link != null) {
-        guard(getActiveContent(), content => {
-          content.classList.remove('active');
-        });
+      guard(getActiveContent(), content => {
+        content.classList.remove('active');
+      });
+      if (route !== '') {
         getContent(route).classList.add('active');
       }
     },
   });
 
+  query('.header').classList.toggle('fixed', router.route !== '');
   setLanguage(router.params.lang);
 
   getLinks().forEach(link => {
     link.addEventListener('click', () => {
-      router.move(link.name);
+      router.move(link.dataset.route);
     });
   });
 
-  query('#language-select').addEventListener('click', () => {
-    const nextLanguage = getCurLanguage() === 'EN' ? 'HE' : 'EN';
-    router.setParam('lang', nextLanguage);
-    setLanguage(nextLanguage);
-  });
+  // query('#language-select').addEventListener('click', () => {
+  //   const nextLanguage = getCurLanguage() === 'EN' ? 'HE' : 'EN';
+  //   router.setParam('lang', nextLanguage);
+  //   setLanguage(nextLanguage);
+  // });
 }
